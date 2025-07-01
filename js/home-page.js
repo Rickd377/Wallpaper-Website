815c08c065d64dd0af4c85fe4deb10610041ffcb
Rick <rick-2007@outlook.com>
2025-07-01 22:14:56 +0200

    dropdown changed and popup added with expand functional

--- js/home-page.js
+++ js/home-page.js
@@ -1,8 +1,12 @@
 document.querySelector(".dropdown-btn").addEventListener("click", (event) => {
-  const dropdownContent = document.querySelector('.dropdown-content');
   const btn = event.currentTarget;
+  const dropdownContent = document.querySelector('.dropdown-content');
+  const icon = btn.querySelector(".dropdown-icon");
   btn.classList.toggle("clicked");
   dropdownContent.classList.toggle("open");
+  if (icon) {
+    icon.classList.toggle("rotated", dropdownContent.classList.contains("open"));
+  }
 });
 
 document.querySelectorAll(".dropdown-content .option").forEach(option => {
@@ -25,3 +29,45 @@ function createImages() {
 }
 
 createImages();
+
+document.querySelector(".image-container").addEventListener("click", function(e) {
+  if (e.target.tagName === "IMG") {
+    const popup = document.querySelector(".image-popup");
+    const popupContainer = popup.querySelector(".popup-container");
+    const imageFunctions = popup.querySelector(".image-functions");
+    popup.style.display = "flex";
+    document.body.style.overflow = "hidden";
+    let popupImg = popupContainer.querySelector("img");
+    if (!popupImg) {
+      popupImg = document.createElement("img");
+      popupContainer.insertBefore(popupImg, imageFunctions);
+    }
+    popupImg.src = e.target.src;
+  }
+});
+
+document.querySelector(".image-popup").addEventListener("click", function(e) {
+  if (e.target.classList.contains("close-popup")) {
+    this.style.display = "none";
+    document.body.style.overflow = "";
+  }
+});
+
+document.querySelector(".favorite-btn").addEventListener("click", function(e) {
+
+});
+
+document.querySelector(".download-btn").addEventListener("click", function(e) {
+
+});
+
+document.querySelector(".share-btn").addEventListener("click", function(e) {
+
+});
+
+document.querySelector(".fullsize-btn").addEventListener("click", function(e) {
+  const popupImg = document.querySelector(".popup-container img");
+  if (popupImg && popupImg.src) {
+    window.open(popupImg.src, "_blank")
+  }
+});
